12dcdb83702a79f2d186244765640ea996fcc3b7
Simon Reinsperger <reinsperger.smn@gmail.com>
2017-04-22 21:28:04 +0200

    chore: check count of last brakman warnings

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ruby:2.4.1
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs jq
 RUN mkdir /usr/src/app
 WORKDIR /usr/src/app
 ADD Gemfile /usr/src/app/Gemfile
--- bin/commit-stage.sh
+++ bin/commit-stage.sh
@@ -4,9 +4,24 @@ bundle install
 rails db:setup
 rails test:controllers
 rails test:models
-brakeman -z
-#brakeman -z -q -o brakeman-output.json
 
-#LAST_ERROR_COUNT=$( cat brakeman-output.json | jq .scan_info.security_warnings )
-#
-#echo ${LAST_ERROR_COUNT}
+git checkout brakeman
+
+brakeman -z -q -o output.json
+
+ERROR_COUNT=$( cat output.json | jq .scan_info.security_warnings )
+LAST_ERROR_COUNT=$( cat last-output.json | jq .scan_info.security_warnings )
+
+if [ ${ERROR_COUNT} -gt ${LAST_ERROR_COUNT} ]
+then
+    exit 0
+fi
+
+rm last-output.json
+mv output.json last-output.json
+
+git add .
+git commit -m "brakeman output log"
+git push -u origin brakeman
+
+git checkout master
